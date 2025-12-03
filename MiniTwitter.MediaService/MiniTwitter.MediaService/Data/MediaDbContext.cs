7bcb6a6381378656bbe5fb6da6d549301ef3cdbd
m.faraji9841@gmail.com <m.faraji9841@gmail.com>
2025-12-03 13:52:53 +0330

    Writing MediaDbContext

---
+++ MiniTwitter.MediaService/MiniTwitter.MediaService/Data/MediaDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniTwitter.MediaService.Models;
+
+namespace MiniTwitter.MediaService.Data
+{
+    public class MediaDbContext : DbContext
+    {
+        public MediaDbContext(DbContextOptions<MediaDbContext> options) : base(options)
+        {
+        }
+        public DbSet<FileRecord> Files { get; set; } = null!;
+    }
+}
---
+++ MiniTwitter.MediaService/MiniTwitter.MediaService/Models/FileRecord.cs
@@ -0,0 +1,20 @@
+﻿namespace MiniTwitter.MediaService.Models
+{
+    public class FileRecord
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string FileName { get; set; } = string.Empty;
+
+        public string Url { get; set; } = string.Empty;
+
+        public string ContentType { get; set; } = string.Empty;
+
+        public long Size { get; set; }
+
+        public string UploadedBy { get; set; } = string.Empty;
+
+        public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
